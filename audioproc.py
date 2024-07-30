477d929ef8c7653f5090058baff01b1ed78a5caa
alina bazhura <bazhura@users.noreply.github.com>
2024-07-30 23:03:08 +0200

    Add files via upload

--- audioproc.py
+++ audioproc.py
@@ -7,117 +7,154 @@ from scipy import io, signal
 from piano import draw_piano
 from pitch import freq_to_number, pitch_name
 
-#AUDIO_FILE = "samples/alina.wav"
-AUDIO_FILE = "samples/piano_c_major_scale.wav"
-SCREEN_WIDTH = 1248
-SCREEN_HEIGHT = 450
-FPS = 30 #Frames fer Second
-FFT_WINDOW_SECONDS = 0.5 # how many seconds of audio make up an FFT window sample
+import matplotlib.pyplot as plt
+
+#AUDIO_FILE = "audio/piano_c_major_scale.wav"
+AUDIO_FILE = "audio/alina_dry.wav"
+
+SCREEN_WIDTH        = 1248 # 24 * 52 (count of white keys)
+SCREEN_HEIGHT       = 450
+FPS                 = 30
+
+FFT_WINDOW_SECONDS  = 0.25 # how many seconds of audio make up an FFT window
+PLAY_ORIGINAL       = True
+TOP_NOTES_COUNT     = 2
+FREQ_LOWEST         = 27
+FREQ_HIGHEST        = 4200
 
 def load_audio_data(audio_file):
-    sample_rate, data = io.wavfile.read(audio_file) # load the data
-
+    # Read data from file
+    sample_rate, data = io.wavfile.read(audio_file)
     print(f"Sample rate: {sample_rate}, type: {data.dtype}, duration: {len(data)/sample_rate}")
 
+    # Normalize data to range -1.0 .. +1.0
+    # See: https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
     if data.dtype == "int32":
-        data = data.astype(np.float32) / (2**31)
+        data = data.astype(np.float64) / (2**31)
+        data = data.astype(np.float32)
     elif data.dtype == "int16":
         data = data.astype(np.float32) / (2**15)
+    elif data.dtype == "uint8":
+        data = (data.astype(np.float32) - 127.5) / 127.5
+    else:
+        data = data.astype(np.float32)
 
-    ch = np.array(data.T[0]) # this is a two channel soundtrack, get the first channel
+    return sample_rate, data
 
-    return sample_rate, data, ch
+def play_tone(freq, time, volume = 0.5):
+    count = int(44100*time)
+    ramp = int(count*0.2)
 
-def extract_sample(audio_array, sample_rate, frame_number):
-    '''Cuts audio into chunks / windows for the FFT to process. 
-        It is not recommended to put all the data into FFT at once'''
-    audio_length = len(audio_array)/sample_rate # seconds
-    frame_count = int(audio_length*FPS) # how many 
-    frame_interval = int(audio_length/frame_count) # data points
-    fft_window_size = int(sample_rate * FFT_WINDOW_SECONDS) # data points
+    buff = np.sin(2 * np.pi * np.arange(count) * freq / 44100)
 
-    begin = frame_number * frame_interval
-    end = begin + fft_window_size
+    ramp_effect = np.concatenate((np.linspace(0, 1, ramp, False),
+                                  np.full((count - 2*ramp,), 1, dtype=float),
+                                  np.linspace(1, 0, ramp, False)))
+    buff = buff * ramp_effect * volume
 
-    return audio_array[begin:end]
+    #plt.plot(buff)
+    #plt.show()
 
-#rfft_freq = np.fft.rfftfreq(fft_window_size, 1/sample_rate)
-
-def play_tone(freq, time):
-    buffer = np.sin(2 * np.pi * np.arange(44100*time) * freq / 44100).astype(np.float32)
-    sound = pygame.mixer.Sound(buffer * 0.2)
+    sound = pygame.mixer.Sound(buff.astype(np.float32))
     sound.play(0)
 
 if __name__ == "__main__":
-    pygame.mixer.init(frequency=44100, size=32, channels=2)
+    sample_rate, audio_data = load_audio_data(AUDIO_FILE)
+
+    pygame.mixer.init(frequency=sample_rate, size=32, channels=2)
     pygame.init()
     pygame.display.set_caption('Alina PyPiano')
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    font = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 30)
- 
-    sample_rate, audio_data, audio_channel = load_audio_data(AUDIO_FILE)
-    sound = pygame.mixer.Sound(audio_data)
-    sound.play(0)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock  = pygame.time.Clock()
+    font   = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 30)
 
-    frame_step = (sample_rate / FPS) # audio samples per video frame
-    fft_window_size = int(sample_rate * FFT_WINDOW_SECONDS) # data points
-    audio_duration = int(len(audio_channel) / sample_rate)
+    # Play original sound
+    if PLAY_ORIGINAL:
+        sound = pygame.mixer.Sound(audio_data)
+        sound.play(0)
 
-    # Hanning Window function - see https://www.sciencedirect.com/topics/engineering/hanning-window
-    window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, fft_window_size, False)))
+    # Get the first channel of multi-channel audio
+    audio_channel = np.array(audio_data.T[0])
 
-    for frame_number in range(audio_duration*FPS):
-        sample = extract_sample(audio_channel, sample_rate, frame_number)
+    SAMPLES_IN_WINDOW = int(sample_rate * FFT_WINDOW_SECONDS)
+    AUDIO_DURATION    = int(len(audio_channel) / sample_rate)
+    FRAME_SAMPLES     = int(sample_rate / FPS) # audio samples per video frame
 
-        fhat = np.fft.fft(sample * window)
-        PSD = (fhat * np.conj(fhat) / fft_window_size).real
+    # Hanning window function
+    window_effect = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_IN_WINDOW, False)))
+    #plt.plot(window_effect)
+    #plt.show()
 
-        freq = (sample_rate / fft_window_size) * np.arange(fft_window_size)
+    for current_frame in range(AUDIO_DURATION*FPS):
+        ###########################
+        # Data processing
+        ###########################
 
-        PSD  = PSD[0:4200]
-        freq = freq[0:4200]
+        # Cut the window
+        window_start = FRAME_SAMPLES * current_frame
+        window = audio_channel[window_start : window_start + SAMPLES_IN_WINDOW]
 
+        # Apply FFT, get power spectral density
+        fhat = np.fft.rfft(window * window_effect)
+        PSD = (fhat * np.conj(fhat) / SAMPLES_IN_WINDOW).real
+
+        freq = (sample_rate / SAMPLES_IN_WINDOW) * np.arange(SAMPLES_IN_WINDOW)
+
+        # Pick frequencies of real piano
+        PSD  = PSD[FREQ_LOWEST:FREQ_HIGHEST]
+        freq = freq[FREQ_LOWEST:FREQ_HIGHEST]
+
+        # Find power peaks in FFT output
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
-        lst, _ = signal.find_peaks(PSD, height=0.5, distance=10)
+        peak_indexes, _ = signal.find_peaks(PSD, height=0.5, distance=10)
 
-        if len(lst):
-            maxfreq_id = lst[0]
-            maxfreq = freq[maxfreq_id]
-            #play_tone(maxfreq, 1/4)
+        ###########################
+        # Drawing in PyGame
+        ###########################
 
-        # Draw in PyGame
-        screen.fill((0,0,0))
-
+        # Draw PSD chart
         PSD_draw = 250-PSD*10
         freq_draw = freq * (SCREEN_WIDTH / len(freq))
 
         chart_data = np.vstack((freq_draw, PSD_draw)).T
         pygame.draw.aalines(screen, "lime", False, chart_data)
 
-        for x in lst:
+        # Highlight all peaks
+        for x in peak_indexes:
             pygame.draw.circle(screen, "blue", (freq_draw[x], PSD_draw[x]), 3, 2)
  
+        # Pick N first peaks
         active_notes = []
-        for x in lst[0:1]:
+        for x in peak_indexes[0:TOP_NOTES_COUNT]:
             f = freq[x]
-            n = freq_to_number(f)
-            n0 = int(round(n))
-            name = pitch_name(n0)
-            active_notes.append(n0-21)
-            #print(f"Frequency: {f} Hz, Number: {n0}, Note: {name}")
-            label, lbl_rect = font.render(name, "white", size=16)
+            key_number = int(round(freq_to_number(f)))
+            name = pitch_name(key_number)
+            active_notes.append(key_number - 21)
+
+            # Draw pitch name
+            label, _ = font.render(name, "white", size=16)
             screen.blit(label, (freq_draw[x], 260))
 
+            # Console feedback
+            print(f"Frequency: {f} Hz, Number: {key_number}, Pitch: {name}")
+
+            # Audio feedback
+            if not PLAY_ORIGINAL:
+                tone_volume = np.clip(PSD[x]/20, 0, 0.7)
+                play_tone(f, 1/10, tone_volume)
+
+        # Draw piano
         piano = draw_piano((SCREEN_WIDTH, 150), active_notes)
         screen.blit(piano, (0, SCREEN_HEIGHT-150))
 
-        #x_window_scale = width / len(window)
-        #window_data = np.vstack((np.arange(len(window)) * x_window_scale, 250-window*250)).T
-        #pygame.draw.aalines(screen, "white", False, window_data)
-
+        ###########################
         # PyGame loop maintenance
+        ###########################
+
+        # Diisplay current frame
         pygame.display.flip()
+
+        # Process PyGame events
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit(0)
@@ -125,4 +162,15 @@ if __name__ == "__main__":
                 if event.key == K_ESCAPE:
                     sys.exit(0)
 
+        # Limit FPS
         clock.tick(FPS)
+        # Clear screen for the next frame
+        screen.fill((0,0,0))
+
+# TODO:
+# - Realtime input from microphone
+# - Adjust frequency chart to align with piano on X axis
+# - Make piano key highlight color depend on PSD
+# - Click piano key -> generate tone
+# - Check if FREQ_LOWEST/FREQ_HIGHEST is applied correctly
+#
--- piano.py
+++ piano.py
@@ -1,25 +1,27 @@
 import pygame
 
 def draw_piano(size, active_notes):
-    #print(active_notes)
     surface = pygame.Surface(size)
 
     # Most modern pianos have a row of 52 white keys and 36 black keys, 88 total
     TOTAL_KEYS = 88
     TOTAL_WHITE_KEYS = 52
+    TOTAL_BLACK_KEYS = 36
+
     WHITE_KEY_WIDTH = size[0] / TOTAL_WHITE_KEYS
     BLACK_KEY_WIDTH = WHITE_KEY_WIDTH * 2/3
     WHITE_KEY_HEIGHT = size[1]
     BLACK_KEY_HEIGHT = WHITE_KEY_HEIGHT * 5/7
+
     OCTAVE_COUNT = 7
 
-    black_keys_pattern = [0, 1, 0] + [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0] * OCTAVE_COUNT + [0]
+    black_keys = [0, 1, 0] + [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0] * OCTAVE_COUNT + [0]
 
     white = 0
     for k in range(TOTAL_KEYS):
         is_active = k in active_notes
 
-        if not black_keys_pattern[k]:
+        if not black_keys[k]:
             key_rect = [white * WHITE_KEY_WIDTH, 0, 
                         WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT]
             pygame.draw.rect(surface, "lime" if is_active else "white", key_rect)
@@ -29,7 +31,7 @@ def draw_piano(size, active_notes):
     white = 0
     for k in range(TOTAL_KEYS):
         is_active = k in active_notes
-        if black_keys_pattern[k]:
+        if black_keys[k]:
             key_rect = [white * WHITE_KEY_WIDTH - BLACK_KEY_WIDTH/2, -4,
                         BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT]
             pygame.draw.rect(surface, 'red' if is_active else 'black', key_rect, 0, 3)
